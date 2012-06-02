aa1d067c40fe338b47aef23cd6cbae438e1fc718
GerJan TeDorsthorst <gerjantd@gmail.com>
2012-06-02 20:58:19 +0200

    After adding Quux.java

--- baz/src/main/java/foo/bar/model/Quux.java
+++ baz/src/main/java/foo/bar/model/Quux.java
@@ -1,23 +1,3 @@
-vanilla-appfuse-2.1.0-spring-mvc
-================================
-
-Vanilla Appfuse 2.1.0 project (Spring MVC + Hibernate), including full-source, templates and eclipse project files
-
-mvn archetype:generate -B -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-spring-archetype -DarchetypeVersion=2.1.0 -DgroupId=foo.bar -DartifactId=baz -DarchetypeRepository=http://oss.sonatype.org/content/repositories/appfuse
-
-# use maven 2.2.1 not 3.0.4 for full-source
-cd baz
-/opt/apache-maven-2.2.1/bin/mvn appfuse:full-source
-
-mvn appfuse:copy-templates
-cd src/test/resources/appfuse/model/
-sed -i -e "s/generateJoinColumnsAnnotation(property/generateJoinColumnsAnnotation(property,cfg/" Ejb3PropertyGetAnnotation.ftl 
-cd ../../../../..
-
-mvn org.apache.maven.plugins:maven-eclipse-plugin:2.6:eclipse
-
-vi src/main/java/foo/bar/model/Quux.java
-# insert
 package foo.bar.model;
 
 import org.apache.commons.lang.builder.*;
@@ -120,4 +100,3 @@ public class Quux extends BaseObject {
     }
 
 }
-# end insert
